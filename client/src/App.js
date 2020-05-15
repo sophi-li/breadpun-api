c9370470a24ae3d977b61792f512a3136011e42c
Sophia Li <sophiali124@gmail.com>
2020-05-14 19:03:24 -0700

    add client

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
-
+import style from "./App.module.css";
+import breadpunPhoto from "./Breadpun.jpg";
 const App = () => {
   const [breadpun, setBreadPun] = useState([]);
 
@@ -11,9 +11,32 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Bread Puns</h1>
-      {breadpun}
+    <div className={style.container}>
+      <h1 className={style.title}>Bread Puns API</h1>
+      <div>{breadpun}</div>
+      <img
+        src={breadpunPhoto}
+        alt="ourdough bread cross section"
+        className={style.breadpunPhoto}
+      ></img>
+
+      <h2 className={style.endpointsHeader}>Endpoints</h2>
+
+      <section className={style.getContainer}>
+        <h3 className={style.getHeader}>GET /api/breadpuns</h3>
+        <p>Returns a single random bread pun.</p>
+        <a href="https://my-bao-server.herokuapp.com/api/breadpuns">
+          https://my-bao-server.herokuapp.com/api/breadpuns
+        </a>
+      </section>
+
+      <section className={style.getContainer}>
+        <h3 className={style.getHeader}>GET /api/allbreadpuns</h3>
+        <p>Returns an array of all bread puns.</p>
+        <a href="https://my-bao-server.herokuapp.com/api/allbreadpuns">
+          https://my-bao-server.herokuapp.com/api/allbreadpuns
+        </a>
+      </section>
     </div>
   );
 };
